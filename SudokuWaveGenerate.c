c2aa8f1606cc7871cc44e7f75e2f866c7b537737
UandWhatArmy <43387245+UandWhatArmy@users.noreply.github.com>
2023-03-11 14:39:49 -0500

    added void initGrid(void);

--- SudokuWaveGenerate.c
+++ SudokuWaveGenerate.c
@@ -2,31 +2,78 @@
 #include <math.h>
 #include <stdlib.h>
 
-void generateFirstGrid(void) {
+int solvedGrid[9][9]; //[rows][collumns]
 
-	int grid[9][9];
-	int nums[9];
 
-	for (int i = 0; i < 9; ++i){
-		nums[i] =  
-	}
-
-	// FILE *fopen(file, w+);
-
-	for (int i = 0; i < 9; ++i) {
-		for (int j = 0; j < 9; ++j) {
-				grid[(j / 3) + (i / 3)][(j % 3) + (i % 3)] = ;
-				printf("%d", grid[(j / 3) + (i / 3)][(j % 3) + (i % 3)]);
-		}
-		printf("\n");
-	}
-
-}
+void initGrid(void);
+void printGrid(void);
+void generateGrid(int startNum, int xPos, int yPos);
+void recurseGen(int xPos, int yPos);
+int * checkInvalidNums(int xPos, int yPos);
 
 int main(void) {
-
-	generateFirstGrid();
+	initGrid();
+	generateGrid(4, 8, 8);
+	printGrid();
 
 	return 0;
 }
 
+void printGrid(void) {		// prints grid matrix to terminal
+	for (int i = 0; i < 9; ++i) {
+		for (int j = 0; j < 9; ++j){
+			if (!(j % 3) && j != 0)
+				printf("| ");
+			printf("%d ", solvedGrid[i][j]);
+		}
+		if (!((i + 1) % 3) && i != 8)
+			printf("\n_____________________\n");
+		else
+			printf("\n");
+	}
+
+}
+
+void initGrid(void) {
+	for (int i = 0; i < 9; ++i) {
+		for (int j = 0; j < 9; ++j){
+			solvedGrid[i][j] = 0;
+		}
+	}
+}
+
+void generateGrid(int startNum, int xPos, int yPos) {	// gets generation started by inserting a starting val and calling the recursion function
+	solvedGrid[xPos][yPos] = startNum;
+	if (xPos > 0) {
+		recurseGen(xPos - 1, yPos);
+	}
+	if (yPos > 0) {
+		recurseGen(xPos, yPos - 1);
+	}
+
+}
+
+void recurseGen(int xPos, int yPos){
+	// printf("x: %d\ty: %d\n", xPos, yPos);
+	int * numArr = checkInvalidNums(xPos, yPos);
+	for (int i = 0; i < (sizeof(numArr) / sizeof(int)); ++i){
+		printf("%d\t", numArr[i]);
+	}
+	printf("\n");
+}
+
+int * checkInvalidNums(int xPos, int yPos) {
+	int * numArr = malloc(0);
+	int numOfInvalid = 0;
+
+	for (int i = 0; i < 9; ++i){
+		if (solvedGrid[i][yPos] != 0){
+			numArr[numOfInvalid] = solvedGrid[i][yPos];
+			++numOfInvalid;
+			numArr = realloc(numArr, sizeof(int) * numOfInvalid);
+			// printf("invalid %d", solvedGrid[i][yPos]);
+		}
+	}
+
+	return numArr;
+}
