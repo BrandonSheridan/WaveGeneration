83e052dc04e0e6c104a80037d8844bc3d02c04f0
UandWhatArmy <bhsheridan2@gmail.com>
2023-03-16 21:21:06 -0400

    holy shit I want to shoot myself, fixed all memory leaks though

--- SudokuWaveGenerate.c
+++ SudokuWaveGenerate.c
@@ -5,7 +5,6 @@
 
 //globals
 int solvedGrid[9][9]; //[rows][collumns]
-// int * numArr;
 time_t seed;
 FILE * out;
 
@@ -20,7 +19,7 @@ int cmpNum(int * numArr, int num);
 int main(int argc, char * argv[]) {
 	initGrid();
 	seed = time(NULL); 
-	srand(0);
+	
 	
 	if (argv[1] == NULL) {
 		printf("no target file specified\n");
@@ -28,11 +27,15 @@ int main(int argc, char * argv[]) {
 	}
 	out = fopen(argv[1], "w");
 	
-	// numArr = malloc(sizeof(int) * 9);
-	
-	recurseGen(0, 0);
-	printf("why crash here\n");
-	
+	int isDone;
+	do {
+		initGrid();
+		srand(seed);
+		isDone = recurseGen(0, 0);
+		seed *= (seed + 1);
+	} while (isDone <= 0);
+
+	printGrid();
 	// free(numArr);
 	// fclose(out);
 
@@ -62,25 +65,24 @@ void initGrid(void) {
 	}
 }
 
-void generateGrid() {	// gets generation started by inserting a starting val and calling the recursion function
-	solvedGrid[0][0] = (rand() % 9 ) + 1;
-	recurseGen(1, 0);printf("returning from generategrid1\n");
-	recurseGen(0, 1);printf("returning from generategrid2\n");
+// void generateGrid() {	// gets generation started by inserting a starting val and calling the recursion function
+// 	solvedGrid[0][0] = (rand() % 9 ) + 1;
+// 	recurseGen(1, 0);printf("returning from generategrid1\n");
+// 	recurseGen(0, 1);printf("returning from generategrid2\n");
 		
-	return;
-}
+// 	return;
+// }
 
 int recurseGen(int xPos, int yPos){
-	// printf("x: %d\ty: %d\n", xPos, yPos);
+	int returnVal = 0;
+	
 	if (solvedGrid[xPos][yPos] > 0) return 0;
-
 
 	int * numArr = checkInvalidNums(xPos, yPos);
 	
 	if (numArr == NULL){
-		// printf("returning from recurseGen\n");
 		free(numArr);
-		return -1;
+		return -1; 
 	}
 	int flag, randNum; //set flag to 0 if a value that is valid is stored
 	
@@ -89,36 +91,30 @@ int recurseGen(int xPos, int yPos){
 		flag = cmpNum(numArr, randNum);
 	} while (flag == 1);
 
-	// printf("\n");
 	free(numArr);
 
 	solvedGrid[xPos][yPos] = randNum;
 
 
 	if (xPos == 0 && yPos == 8){
-		printGrid();
-		return 0;
-		// fclose(out);
-		// exit(1);
-	}
+		return 1;
+		}
 
 	if (xPos < 8) {
-		if (recurseGen(xPos + 1, yPos) == -1)
-			return -1;
+		returnVal += recurseGen(xPos + 1, yPos);
 	}
 
 	if (yPos < 8) {
-		if (recurseGen(xPos, yPos + 1) == -1)
-			return -1;
+		returnVal += recurseGen(xPos, yPos + 1);
 	}
 
-
+	return returnVal;
 }
 
 int * checkInvalidNums(int xPos, int yPos) {
 	int * numArr = malloc(9 * sizeof(int));
 	if (numArr == NULL){
-		printf("malloc failed exiting program");
+		printf("malloc failed, exiting\n");
 		exit(1);
 	}
 	for (int i = 0; i < 9; ++i) { //init array
@@ -128,32 +124,24 @@ int * checkInvalidNums(int xPos, int yPos) {
 	for (int i = 0; i < 9; ++i){
 		if (solvedGrid[i][yPos] != 0){
 			numArr[solvedGrid[i][yPos] - 1] = 1;
-			// printf("%d found at x: %d y: %d\n", solvedGrid[i][yPos], i, yPos);
-			// printf("invalid %d", solvedGrid[i][yPos]);
 		}
 	}
 
 	for (int i = 0; i < 9; ++i){
 		if (solvedGrid[xPos][i] != 0){
-			// printf("%d found at x: %d y: %d\n", solvedGrid[xPos][i], xPos, i);
 			numArr[solvedGrid[xPos][i] - 1] = 1;
 		}
 	}
 
 	for (int i = 0; i < 3; ++i) {
 		for (int j = 0; j < 3; ++j) {
-			// printf("checking x: %d y: %d\n",i + (xPos / 3) * 3, j + (yPos / 3) * 3);
 			if (solvedGrid[i + xPos - (xPos % 3)][j + yPos - (yPos % 3)] != 0){
-				// printf("%d found at x: %d y: %d\n", solvedGrid[i + xPos - (xPos % 3)][j + yPos - (yPos % 3)], i + (xPos / 3) * 3, j + (yPos / 3) * 3);
 				numArr[solvedGrid[i + xPos - (xPos % 3)][j + yPos - (yPos % 3)] - 1] = 1;
 			}
 		}
 	}
 
-	// for (int i = 0; i < 9; ++i){
-	// 	printf("%d ", numArr[i]);
-	// }
-	// printf("\n");
+	// printGrid();
 
 	for (int i = 0; i < 9; ++i){
 		if(numArr[i] == 1){}
@@ -161,11 +149,8 @@ int * checkInvalidNums(int xPos, int yPos) {
 			return numArr;
 		}
 	} 
-
-	printf("invalid grid: retrying\n");
-	printGrid();
-	initGrid();
-	seed *= seed + seed;
+	free(numArr);
+	// printf("invalid grid: retrying\n");
 	return NULL;
 }
 
